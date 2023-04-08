060bfb2c601db687077b7c75c9bb9e8b2a169abe
Evgeniy Poleshchuk <2x2great@gmail.com>
2023-04-08 12:52:05 +0300

    Maim

--- src/test/java/MethodTest.java
+++ src/test/java/MethodTest.java
@@ -1,6 +1,9 @@
+import org.hamcrest.generator.HamcrestFactoryWriter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,7 +18,7 @@ class MethodTest {
     void tax() {
         int expend = 36;
         int actual = Method.tax(600);
-        Assertions.assertEquals(expend,actual);
+        assertThat(expend,equalTo(actual));
 
     }
 
